0b6396322b700ca9fc3529d1cb27f65586a1b956
m0t0iml <motorbreath6666@gmail.com>
2024-02-05 10:39:38 +0300

    warning1

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 600,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/catalog/left_solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/catalog/right_solid.png"></button>',
+      });
+  });
