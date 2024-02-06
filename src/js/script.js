ef31fb249c5812bcdb04f3c4bcd4c97a1768ebae
m0t0iml <motorbreath6666@gmail.com>
2024-02-06 15:42:20 +0300

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 600,
         // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/catalog/left_solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/catalog/right_solid.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/catalog/left_solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/catalog/right_solid.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+              },
+        ]
       });
   });
